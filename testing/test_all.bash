0250bea9299c533d831680d959967fd58a881eb5
AndSte01 <steger.andreas.2707@gmail.com>
2022-09-15 18:17:25 +0200

    renamed output file to prevent confusion

--- testing/test_all.bash
+++ testing/test_all.bash
@@ -2,28 +2,28 @@
 # run all test scripts and write output to file
 
 # setup
-FORCE_COLOR=true hopp test setup.json -e env.json | tee results.ansi
+FORCE_COLOR=true hopp test setup.json -e env.json | tee test_results.ansi
 
 # users
-FORCE_COLOR=true hopp test users.json -e env.json | tee -a results.ansi
+FORCE_COLOR=true hopp test users.json -e env.json | tee -a test_results.ansi
 
 # clean
 hopp test setup.json -e env.json > /dev/null
 
 # competitions
-FORCE_COLOR=true hopp test competitions.json -e env.json | tee -a results.ansi
+FORCE_COLOR=true hopp test competitions.json -e env.json | tee -a test_results.ansi
 
 # clean
 hopp test setup.json -e env.json > /dev/null
 
 # disciplines
-FORCE_COLOR=true hopp test disciplines.json -e env.json | tee -a results.ansi
+FORCE_COLOR=true hopp test disciplines.json -e env.json | tee -a test_results.ansi
 
 # clean
 hopp test setup.json -e env.json > /dev/null
 
 # results
-FORCE_COLOR=true hopp test results.json -e env.json | tee -a results.ansi
+FORCE_COLOR=true hopp test results.json -e env.json | tee -a test_results.ansi
 
 # clean
 hopp test setup.json -e env.json > /dev/null
@@ -32,7 +32,7 @@ hopp test setup.json -e env.json > /dev/null
 hopp test dummy_data.json -e env.json > /dev/null
 
 # poll
-FORCE_COLOR=true hopp test poll.json -e env.json | tee -a results.ansi
+FORCE_COLOR=true hopp test poll.json -e env.json | tee -a test_results.ansi
 
 # clean
 hopp test setup.json -e env.json > /dev/null
